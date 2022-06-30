1da4af9ccad19c4007834ecbf407bc71dfbd66d0
Zhe <zchen@sps-program.com>
2022-06-30 19:29:07 +0000

    Add javadoc

--- portfolio/src/main/java/com/google/sps/servlets/greetingServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/greetingServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 
+/** Servlet responsible for displaying greetings. */
 @WebServlet("/greeting")
 public class greetingServlet extends HttpServlet {
 
